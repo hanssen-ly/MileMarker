3d5b7cc6cad02ea407e4bc76c67089f038c8631b
hanssen-ly <hanssen0307@gmail.com>
2025-12-28 17:59:44 -0500

    Completed MileMarker

---
+++ main.py
@@ -0,0 +1,348 @@
+# Imports
+from PyQt5.QtCore import Qt, QDate
+from PyQt5.QtWidgets import QApplication, QDateEdit, QLineEdit, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView, QCheckBox
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+import numpy as np
+from sys import exit
+
+# main
+class MileMarker(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.settings()
+        self.initUI()
+        self.button_click()
+
+    # settings
+    def settings(self):
+        self.setWindowTitle("MileMarker")
+        self.resize(800,600)
+    # init ui
+    def initUI(self):
+        self.date_box = QDateEdit()
+        self.date_box.setDate(QDate.currentDate())
+
+        self.kal_box = QLineEdit()
+        self.kal_box.setPlaceholderText("Number of Burned Calories")
+
+        self.distance_box = QLineEdit()
+        self.distance_box.setPlaceholderText("Enter distance ran")
+
+        self.description = QLineEdit()
+        self.description.setPlaceholderText("Enter a description")
+
+
+        self.submit_btn = QPushButton("Submit")
+        self.add_btn = QPushButton("Add")
+        self.delete_btn = QPushButton("Delete")
+        self.clear_btn = QPushButton("Clear")
+        self.dark_mode = QCheckBox("Dark Mode")
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(5)
+        self.table.setHorizontalHeaderLabels(["ID", "Date", "Calories", "Distance", "Description"])
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        # layout design
+        self.master_layout = QHBoxLayout()
+        self.col1 = QVBoxLayout()
+        self.col2 = QVBoxLayout()
+
+        self.sub_row1 = QHBoxLayout()
+        self.sub_row2 = QHBoxLayout()
+        self.sub_row3 = QHBoxLayout()
+        self.sub_row4 = QHBoxLayout()
+
+        self.sub_row1.addWidget(QLabel("Date:"))
+        self.sub_row1.addWidget(self.date_box)
+        self.sub_row2.addWidget(QLabel("Calories:"))
+        self.sub_row2.addWidget(self.kal_box)
+        self.sub_row3.addWidget(QLabel("Miles:"))
+        self.sub_row3.addWidget(self.distance_box)
+        self.sub_row4.addWidget(QLabel("Description:"))
+        self.sub_row4.addWidget(self.description)
+
+        self.col1.addLayout(self.sub_row1)
+        self.col1.addLayout(self.sub_row2)
+        self.col1.addLayout(self.sub_row3)
+        self.col1.addLayout(self.sub_row4)
+        self.col1.addWidget(self.dark_mode)
+
+        btn_row1 = QHBoxLayout()
+        btn_row2 = QHBoxLayout()
+
+        btn_row1.addWidget(self.add_btn)
+        btn_row1.addWidget(self.delete_btn)
+        btn_row2.addWidget(self.submit_btn)
+        btn_row2.addWidget(self.clear_btn)
+
+        self.col1.addLayout(btn_row1)
+        self.col1.addLayout(btn_row2)
+
+        self.col2.addWidget(self.canvas)
+        self.col2.addWidget(self.table)
+
+
+        self.master_layout.addLayout(self.col1, 30)
+        self.master_layout.addLayout(self.col2, 70)
+        self.setLayout(self.master_layout)
+
+        self.apply_styles()
+        self.load_table()
+    # events
+    def button_click(self):
+        self.add_btn.clicked.connect(self.add_workout)
+        self.delete_btn.clicked.connect(self.delete_workout)
+        self.submit_btn.clicked.connect(self.calculate_calories)
+        self.dark_mode.stateChanged.connect(self.toggle_dark)
+        self.clear_btn.clicked.connect(self.reset)
+    # load tables
+    def load_table(self):
+        self.table.setRowCount(0)
+        query = QSqlQuery("SELECT * FROM fitness ORDER BY date DESC")
+        row = 0
+        while query.next():
+            fit_id = query.value(0)
+            date = query.value(1)
+            calories = query.value(2)
+            distance = query.value(3)
+            description = query.value(4)
+
+            self.table.insertRow(row)
+            self.table.setItem(row, 0, QTableWidgetItem(str(fit_id)))
+            self.table.setItem(row, 1, QTableWidgetItem(date))
+            self.table.setItem(row, 2, QTableWidgetItem(str(calories)))
+            self.table.setItem(row, 3, QTableWidgetItem(str(distance)))
+            self.table.setItem(row, 4, QTableWidgetItem(description))
+            row += 1
+    # add tables
+    def add_workout(self):
+        date = self.date_box.date().toString("yyyy-MM-dd")
+        calories = self.kal_box.text()
+        distance = self.distance_box.text()
+        description = self.description.text()
+
+        query = QSqlQuery("""
+                          INSERT INTO fitness (date, calories, distance, description)
+                          VALUES (?,?,?,?)
+                          """)
+        query.addBindValue(date)
+        query.addBindValue(calories)
+        query.addBindValue(distance)
+        query.addBindValue(description)
+        query.exec_()
+
+        self.date_box.setDate(QDate.currentDate())
+        self.kal_box.clear()
+        self.distance_box.clear()
+        self.description.clear()
+
+        self.load_table()
+    # delete tables
+    def delete_workout(self):
+        selected_row = self.table.currentRow()
+
+        if selected_row == -1:
+            QMessageBox.warning(self,"Error","Please choose a row to delete.")
+
+        fit_id = int(self.table.item(selected_row,0).text())
+        confirm = QMessageBox.question(self,"Are you sure?", "Delete this workout?", QMessageBox.Yes | QMessageBox.No)
+
+        if confirm == QMessageBox.No:
+            return
+
+        query = QSqlQuery()
+        query.prepare("DELETE FROM fitness WHERE id = ?")
+        query.addBindValue(fit_id)
+        query.exec_()
+
+        self.load_table()
+    # calculate calories
+    def calculate_calories(self):
+        distances = []
+        calories = []
+
+        query = QSqlQuery("SELECT distance, calories FROM fitness ORDER BY calories ASC")
+        while query.next():
+            distance = query.value(0)
+            calorie = query.value(1)
+            distances.append(distance)
+            calories.append(calorie)
+
+        try:
+            min_calorie = min(calories)
+            max_calories = max(calories)
+            normalized_calories = [(calorie - min_calorie) / (max_calories - min_calorie) for calorie in calories]
+
+            # plt.style.use("Solarize_Light2")
+            ax = self.figure.subplots()
+            ax.scatter(distances, calories, c=normalized_calories, cmap="magma", label="Data Points")
+            ax.set_title("Distance V.S. Calories")
+            ax.set_xlabel("Distance")
+            ax.set_ylabel("Calories")
+            cbar = ax.figure.colorbar(ax.collections[0], label="Normalized Calories")
+            ax.legend()
+            self.canvas.draw()
+
+        except Exception as e:
+            print("ERROR:{e}")
+            QMessageBox.warning(self,"Error","Please enter some data first.")
+    # styling
+    def apply_styles(self):
+        self.setStyleSheet("""
+            QWidget {
+                background-color: #b8c9e1;
+            }
+
+            QLabel {
+                color: #333;
+                font-size: 14px;
+            }
+
+            QLineEdit, QComboBox, QDateEdit {
+                background-color: #b8c9e1;
+                color: #333;
+                border: 1px solid #444;
+                padding: 5px;
+            }
+
+            QTableWidget {
+                background-color: #c9d6ea;
+                color: #333;
+                gridline-color: #666;
+                border: 1px solid #555;
+                selection-background-color: #a5b8d6;
+            }
+
+            QTableWidget::item {
+                border: 1px solid #666;
+            }
+
+            QHeaderView::section {
+                background-color: #9fb3d9;
+                color: #222;
+                padding: 4px;
+                border: 1px solid #555;
+                font-weight: bold;
+            }
+
+            QTableCornerButton::section {
+                background-color: #9fb3d9;
+                border: 1px solid #555;
+            }
+
+
+            QPushButton {
+                background-color: #9fb3d9;
+                color: #fff;
+                border: none;
+                padding: 8px 16px;
+                font-size: 14px;
+            }
+
+            QPushButton:hover {
+                background-color: #8fa2c7;
+            }
+                           """)
+        figure_color = "#d4c19b"
+        self.figure.patch.set_facecolor(figure_color)
+        self.canvas.setStyleSheet(f"background-color:(figure_color)")
+
+        if self.dark_mode.isChecked():
+            self.setStyleSheet("""
+            QWidget {
+                background-color: #1f2a3a;
+            }
+
+            QLabel {
+                color: #e6e9ef;
+                font-size: 14px;
+            }
+
+            QLineEdit, QComboBox, QDateEdit {
+                background-color: #2b3a52;
+                color: #e6e9ef;
+                border: 1px solid #445a7c;
+                padding: 5px;
+            }
+
+            QTableWidget {
+                background-color: #2b3a52;
+                color: #e6e9ef;
+                gridline-color: #445a7c;
+                border: 1px solid #445a7c;
+                selection-background-color: #3e5c8a;
+            }
+
+            QTableWidget::item {
+                border: 1px solid #445a7c;
+            }
+
+            QHeaderView::section {
+                background-color: #3e5c8a;
+                color: #ffffff;
+                padding: 4px;
+                border: 1px solid #445a7c;
+                font-weight: bold;
+            }
+
+            QTableCornerButton::section {
+                background-color: #3e5c8a;
+                border: 1px solid #445a7c;
+            }
+
+            QPushButton {
+                background-color: #3e5c8a;
+                color: #ffffff;
+                border: none;
+                padding: 8px 16px;
+                font-size: 14px;
+            }
+
+            QPushButton:hover {
+                background-color: #4d6fa3;
+            }
+                            """)
+
+    # dark mode
+    def toggle_dark(self):
+        self.apply_styles()
+    # reset
+    def reset(self):
+        self.date_box.setDate(QDate.currentDate())
+        self.kal_box.clear()
+        self.distance_box.clear()
+        self.description.clear()
+        self.figure.clear()
+        self.canvas.draw()
+# initialize db
+db = QSqlDatabase.addDatabase("QSQLITE")
+db.setDatabaseName("fitness.db")
+
+if not db.open():
+    QMessageBox.critical(None, "ERROR","Can not open the database")
+    exit(2)
+
+query = QSqlQuery()
+query.exec_("""
+            CREATE TABLE IF NOT EXISTS fitness (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT,
+                calories REAL,
+                distance REAL,
+                description TEXT
+            )
+            """)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main = MileMarker()
+    main.show()
+    app.exec_()
